52ae705e741a3b39e310c572486119729720521b
Berci Kormendy <korber314@icloud.com>
2021-07-25 16:05:31 +0200

    dedicated parse function

--- src/Parson.ts
+++ src/Parson.ts
@@ -1,9 +1,22 @@
+import { string } from "yargs";
 import * as CombinatorBase from "./CombinatorBase";
 import { result, zero, bind, plus } from "./CombinatorBase";
 import { seq, sat, item, chr, str, many1, many, between, digit, lower, upper, anyChar, alphanum, letter, space, word, nat, parenthesised, bracketed, sepBy, sepBy1, alternative } from "./ParserCombinators";
 
 export import Parser = CombinatorBase.Parser;
 export import ParserCombinatorResult = CombinatorBase.ParserCombinatorResult;
+
+const parse = <T>(source: string, parser: Parser<T>): T | null => {
+    const result = parser(source);
+    if (result.length === 0) {
+        return null;
+    }
+    if (result[0].rest.length > 0) {
+        // TODO: not fully sure about this
+        return null;
+    }
+    return result[0].result;
+}
 
 const Parson = {
     result,
@@ -31,7 +44,8 @@ const Parson = {
     many,
     many1,
     between,
-    alternative
+    alternative,
+    parse
 };
 
 export default Parson;
